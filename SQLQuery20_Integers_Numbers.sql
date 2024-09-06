fd3b6ff41133080429ab6b4873b46bee9e5a0359
Kiyoshi C. Rio <86674319+Kiyoshi8@users.noreply.github.com>
2024-09-06 19:12:50 +0800

    Add files via upload

---
+++ SQLQuery20_Integers_Numbers.sql
@@ -0,0 +1,32 @@
+Bigint 
+Int - up to 2,000,000, 000
+Tinyint - 0-255
+Smallint - -32767 to 32768
+
+
+BITS
+
+Smallint = 20,000
+
+
+declare @myVar as tinyint = 20000
+select @myVar
+
+255 tinyint positive integers and zero
+32767 smallint negative and positive integers
+int
+bigint
+
+--Initialize a variable, give it a data type and an initial value
+
+DECLARE @myvar AS bigint = 2000
+
+--Multiple that value
+
+STE @myvar = @myvar * @myvar * @myvar
+
+--Retrieve that value
+
+SELECT @myvar AS myVariable
+
+--BITS
